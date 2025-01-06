875c978057911a3f8f387bc2edfbd4f48b41c3d6
PullShark101 <arkajit@contlo.com>
2025-01-06 20:36:23 +0530

    adding main file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from backend.memory.chroma_memory.add_data import add_pdf_to_chroma
 from backend.memory.mem0_memory.try_mem0 import add_memory_in_mem0, extract_relevant_memories
 
 if __name__ == "__main__":
-    add_pdf_to_chroma(
-        pdf_path="/Users/arkajitdatta/Documents/projects/springboard/embedding/pdfs/jaipur_wiki.pdf"
-    )
-    # chat_with_travel_assistant()
+    # add_pdf_to_chroma(
+    #     pdf_path="/Users/arkajitdatta/Documents/projects/springboard/embedding/pdfs/jaipur_wiki.pdf"
+    # )
+    chat_with_travel_assistant()
